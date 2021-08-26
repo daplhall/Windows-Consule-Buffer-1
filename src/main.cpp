ffdd3c8d4356e9885e26f9845d2016da19ddcbf1
Daniel Ploug Hall <danielploug@hotmail.com>
2021-08-26 08:20:57 +0200

    added a canvas class, that can create a screen and test it

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,24 @@
 #include <windows.h>
 #include <iostream>
-// https://gamedev.stackexchange.com/questions/162455/native-c-console-screen-buffer
-/*
-
-    You can set buffer size before setting screen size
-    Notes:
-    This creates a new console scrren buffer and returns it,  here we can specifiy options
-        CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0 , NULL, CONSOLE_TEXTMODE_BUFFER, NULL); 
-    This seems to get a handle, for standard useage, likely just a default config.
-        GetStdHandle(STD_OUTPUT_HANDLE) 
-    This sets the active screen buffer to a wchar sequence. 
-        SetConsoleActiveScreenBuffer
-*/
+#include "Canva.h"
 
 
-void screenTest(wchar_t *screen, int const length)
-{
-    int i = 0;
-    for (wchar_t *ss = screen, *se = screen + length; ss != se; ss++)
-    {
-        if (i > 9){
-            i = 0;
-        }
-        *ss = '0' + i;
-        i++;
-    }
-}
-
-void screenTest1(wchar_t *screen, int const length){
-    for (wchar_t *ss = screen, *se = screen + length; ss != se; ss++){
-        *ss = '#';
-    }
-}
 
 int main(int argc, char *argv[]){
-    int a,b;
-    short ScreenHeight      = 20;
-    short ScreenWidth       = 20;
+    int a;
+    wCanva canva;
+    canva.CreateScreen(120,40);
+    canva.TestCanva();
+    std::cin>>a;
+    return 0;
+}
+
+
+/*
+int main(int argc, char *argv[]){
+    int a;
+    short ScreenWidth       = 120;
+    short ScreenHeight      = 40;
     int   ScreenArrayLength =  ScreenWidth*ScreenHeight;
     COORD coord             = {ScreenWidth, ScreenHeight};
     SMALL_RECT m_rectWindow = { 0, 0, 1, 1};
@@ -58,3 +40,4 @@ int main(int argc, char *argv[]){
     std::cin>>a;
     return 0;
 }
+*/
